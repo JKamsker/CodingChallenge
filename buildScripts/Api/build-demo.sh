abb006bcf189210cb96abbdaf097cca3788e50b9
JKamsker <git.jonas@kamsker.at>
2021-12-10 07:24:09 +0000

    Added scripts

--- buildScripts/Api/build-demo.sh
+++ buildScripts/Api/build-demo.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-# echo "hello world"
 
-cd ../..
+SCRIPT_PATH="$(dirname "$(realpath "$0")")"
+ROOT_PATH="$SCRIPT_PATH/../.."
+cd $ROOT_PATH
 
 docker build -t dcapi . -f ./buildScripts/Api/Dockerfile-Base
 docker build -t dcapi-demo-plugin . -f ./buildScripts/Api/Dockerfile-DemoPlugin
-docker build -t dcapi-matrix-plugin . -f ./buildScripts/Api/Dockerfile-NumberMatrix
+docker build -t dcbot . -f ./buildScripts/Bot/Dockerfile 
+# docker build -t dcapi-matrix-plugin . -f ./buildScripts/Api/Dockerfile-NumberMatrix
 
 docker tag dcapi jkamsker/coding-challenge-api:latest-base
 docker tag dcapi-demo-plugin jkamsker/coding-challenge-api:latest-demo-plugin
